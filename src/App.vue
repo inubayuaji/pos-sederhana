0052e0c33b6b878fcdcfa837ec56bc3a981b2a93
inubayuaji <inubayuaji@gmail.com>
2021-09-28 10:36:30 +0700

    navbar window controls

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <v-app>
-      <v-system-bar
-        window
-        dark
-        app
-        color="#212121"
-      >
+      <v-system-bar window dark app color="#212121">
         <v-spacer></v-spacer>
-        <v-icon small>mdi-window-minimize</v-icon>
-        <v-icon small>mdi-window-maximize</v-icon>
-        <v-icon small>mdi-window-close</v-icon>
+        <v-icon small @click="minimizeWindow()">mdi-window-minimize</v-icon>
+
+        <v-icon v-if="isMaximized" small @click="maximizeWindow()"
+          >mdi-window-restore</v-icon
+        >
+        <v-icon v-else small @click="maximizeWindow()"
+          >mdi-window-maximize</v-icon
+        >
+
+        <v-icon small @click="closeWindow()">mdi-window-close</v-icon>
       </v-system-bar>
 
       <v-navigation-drawer permanent expand-on-hover app>
@@ -35,13 +37,51 @@
           </v-list-item>
         </v-list>
       </v-navigation-drawer>
-      
+
       <v-main>
         <router-view></router-view>
       </v-main>
     </v-app>
   </div>
 </template>
+
+<script>
+const { remote } = require("electron");
+
+export default {
+  name: "App",
+  data() {
+    return {
+      isMaximized: true,
+    };
+  },
+  methods: {
+    getWindow() {
+      return remote.BrowserWindow.getFocusedWindow();
+    },
+    closeWindow() {
+      this.getWindow().close();
+    },
+    maximizeWindow() {
+      const window = this.getWindow();
+
+      if (window.isMaximized()) {
+        this.isMaximized = false;
+        window.unmaximize();
+      } else {
+        this.isMaximized = true;
+        window.maximize();
+      }
+    },
+    minimizeWindow() {
+      this.getWindow().minimize();
+    },
+  },
+  mounted() {
+    this.isMaximized = this.getWindow().isMaximized();
+  },
+};
+</script>
 
 <style scoped>
 html {
