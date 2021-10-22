339216344e185fcfadcfd581bcc92061df939526
inubayuaji <inubayuaji@gmail.com>
2021-10-22 09:56:47 +0700

    add order detail ui

---
+++ src/components/OrderDetail.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-card outlined color="#212121" class="h-100 order-detail">
+    <div class="order-date">
+      <h3>Tanggal</h3>
+      <h3>19-09-2021</h3>
+    </div>
+
+    <hr class="mt-2 mb-5" />
+
+    <div class="order-list">
+      <div class="order-item">
+        <div class="order-item-control">
+          <v-btn icon color="red">
+            <v-icon>mdi-delete-outline</v-icon>
+          </v-btn>
+        </div>
+        <div class="order-item-name">Pensil</div>
+        <div class="order-item-price">Rp <span>10000</span></div>
+        <div class="order-item-total">
+          <input type="number" class="order-item-total-input" value="1" />
+        </div>
+      </div>
+    </div>
+
+    <hr class="mt-2 mb-2" />
+
+    <div class="order-total">
+      <div class="order-total-label">Total</div>
+      <div class="order-total-price">Rp <span>10000</span></div>
+    </div>
+
+    <v-card-actions class="p-0 mt-5">
+      <v-btn color="red">Batal</v-btn>
+      <v-btn color="green">Bayar</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'OrderDetail',
+}
+</script>
+
+<style scoped>
+.order-detail {
+  padding: 15px;
+}
+.order-date {
+  display: flex;
+  justify-content: space-between;
+}
+.order-item,
+.order-total {
+  display: flex;
+}
+.order-item-control {
+  flex-grow: 1;
+}
+.order-item-name {
+  display: flex;
+  align-items: center;
+  flex-grow: 4;
+}
+.order-item-price {
+  display: flex;
+  align-items: center;
+  flex-grow: 2;
+  text-align: right;
+}
+.order-item-total {
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+  text-align: right;
+}
+
+.order-total-label {
+  flex-grow: 5;
+}
+.order-total-price {
+  flex-grow: 3;
+  text-align: right;
+}
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+.order-item-total-input {
+  text-align: right;
+  color: #fff;
+  width: 50px;
+}
+</style>
--- src/views/Kasir.vue
+++ src/views/Kasir.vue
@@ -1,5 +1,129 @@
 <template>
-  <div>
-    <h1>Kasir</h1>
-  </div>
+  <v-container class="h-100">
+    <v-row class="h-100">
+      <v-col cols="8">
+        <v-row>
+          <v-col cols="12">
+            <h1>Kasir</h1>
+          </v-col>
+        </v-row>
+
+        <v-row align="center">
+          <v-col cols="8">
+            <v-row>
+              <v-col cols="4">
+                <v-text-field label="Cari"></v-text-field>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="4">
+            <v-row justify="end">
+              <v-col cols="12" class="d-flex justify-end">
+                <!-- Button -->
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12">
+            <v-data-table
+              :headers="headers"
+              :items="desserts"
+              :items-per-page="5"
+              item-key="name"
+              class="elevation-1"
+              :footer-props="{
+                prevIcon: 'mdi-arrow-left',
+                nextIcon: 'mdi-arrow-right',
+              }"
+            >
+              <template v-slot:item="row">
+                <tr>
+                  <td>{{ row.item.name }}</td>
+                  <td>{{ row.item.category }}</td>
+                  <td class="d-flex justify-end">
+                    <!-- <confirm-hapus></confirm-hapus>
+                    <form-edit></form-edit> -->
+                  </td>
+                </tr>
+              </template>
+            </v-data-table>
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <v-col cols="4" class="h-100">
+        <order-detail></order-detail>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import OrderDetail from '../components/OrderDetail.vue';
+export default {
+  components: { OrderDetail },
+  name: "KasirPage",
+  data() {
+    return {
+      headers: [
+        {
+          text: "Dessert (100g serving)",
+          align: "start",
+          value: "name",
+        },
+        { text: "Category", value: "category" },
+      ],
+      desserts: [
+        {
+          name: "Frozen Yogurt",
+          category: "Ice cream",
+        },
+        {
+          name: "Ice cream sandwich",
+          category: "Ice cream",
+        },
+        {
+          name: "Eclair",
+          category: "Cookie",
+        },
+        {
+          name: "Cupcake",
+          category: "Pastry",
+        },
+        {
+          name: "Gingerbread",
+          category: "Cookie",
+        },
+        {
+          name: "Jelly bean",
+          category: "Candy",
+        },
+        {
+          name: "Lollipop",
+          category: "Candy",
+        },
+        {
+          name: "Honeycomb",
+          category: "Toffee",
+        },
+        {
+          name: "Donut",
+          category: "Pastry",
+        },
+        {
+          name: "KitKat",
+          category: "Candy",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.h-100 {
+  height: 100%;
+}
+</style>
