0db613bce4bf0e57c17aff2890928a4919a5e604
inubayuaji <inubayuaji@gmail.com>
2021-10-25 16:53:47 +0700

    add migration

---
+++ migration.js
@@ -0,0 +1,35 @@
+const knex = require("knex");
+
+const db = knex({
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./src/database.sqlite",
+  },
+});
+
+async function up(db) {
+  await db.schema
+    .createTable("admin", function(table) {
+      table.string("username").primary();
+      table.string("nama");
+      table.string("password");
+    })
+    .createTable("barang", function(table) {
+      table.string("id").primary();
+      table.string("barcode");
+      table.string("nama");
+      table.string("harga").defaultTo(0);
+      table.string("jumlah").defaultTo(0);
+    });
+
+  await db.table("admin")
+    .insert([{ username: "admin", nama: "Admin", password: "admin" }])
+    .then(function() {
+      process.exit();
+    });
+}
+
+(async function() {
+  await up(db);
+})();
