8c6909eb1b262d35e7fb2dd402ad71b33ebfde3c
inubayuaji <inubayuaji@gmail.com>
2021-11-01 12:52:50 +0700

    fix vue router

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,25 +1,25 @@
 <template>
   <v-navigation-drawer permanent expand-on-hover app>
     <v-list nav dense>
-      <v-list-item link to="/">
+      <v-list-item link :to="{ name: 'Kasir' }">
         <v-list-item-icon>
           <v-icon>mdi-cash-register</v-icon>
         </v-list-item-icon>
         <v-list-item-title>Kasir</v-list-item-title>
       </v-list-item>
-      <v-list-item link href="/barang">
+      <v-list-item link :to="{ name: 'Barang' }">
         <v-list-item-icon>
           <v-icon>mdi-package-variant</v-icon>
         </v-list-item-icon>
         <v-list-item-title>Barang</v-list-item-title>
       </v-list-item>
-      <v-list-item link href="/admin">
+      <v-list-item link :to="{ name: 'Admin' }">
         <v-list-item-icon>
           <v-icon>mdi-account-multiple</v-icon>
         </v-list-item-icon>
         <v-list-item-title>Admin</v-list-item-title>
       </v-list-item>
-      <v-list-item link href="/logout">
+      <v-list-item link :to="{ name: 'Logout' }">
         <v-list-item-icon>
           <v-icon>mdi-logout</v-icon>
         </v-list-item-icon>
@@ -31,6 +31,6 @@
 
 <script>
 export default {
-  name: 'SideNav'
-}
+  name: "SideNav",
+};
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,10 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
+    redirect: "/kasir"
+  },
+  {
+    path: "/kasir",
     name: "Kasir",
     component: Kasir,
   },
@@ -31,7 +35,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: process.env.IS_ELECTRON ? "hash" : "history",
   base: process.env.BASE_URL,
   routes,
 });
